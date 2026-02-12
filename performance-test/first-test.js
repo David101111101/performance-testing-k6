b78eac6feab8f1c1c3ca6aac526068925d76462d
Dave <davidstevenabril@gmail.com>
2026-02-12 13:49:54 -0500

    ci: run k6 checks on PR; upload results on main/schedule

--- performance-test/first-test.js
+++
@@ -1,6 +0,0 @@
-import http from "k6/http";
-export default function () {
-    let response = http.get("https://api.escuelajs.co/api/v1/products");
-    console.log("Response status: " + response.status);
-
-}
--- performance-test/load.js
+++ performance-test/load.js
@@ -2,14 +2,12 @@ import http from "k6/http";
 
 /* Load testing to evaluate the performance of the API under different levels of load. without sleep property
 */
-/*629.746 Interations > 723.7 per S, data received 13GB*/
 export const options = {
     stages: [
-        { duration: "3M", target: 100 }, 
-        { duration: "5M", target: 100 },  
-        { duration: "45s", target: 150 },  
-        { duration: "45s", target: 100 }, 
-        { duration: "5M", target: 100 },
+        { duration: "10s", target: 100 }, 
+        { duration: "15s", target: 100 },  
+        { duration: "5s", target: 150 },  
+        { duration: "10s", target: 100 }, 
     ],
 }
 
